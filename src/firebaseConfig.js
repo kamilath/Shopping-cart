8d0bb670c0c634aad397fbcacf7349d2c8b4b88e
Kamilath Rifka S <117097462+kamilath@users.noreply.github.com>
2025-07-04 00:15:23 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyD0V5TH6daEZXMjbVPJGHpNBgJOOacrI4Q",
+  authDomain: "ecommerce-354cd.firebaseapp.com",
+  projectId: "ecommerce-354cd",
+  storageBucket: "ecommerce-354cd.firebasestorage.app",
+  messagingSenderId: "314560577473",
+  appId: "1:314560577473:web:b8d8ecab6f4c91ceaab940",
+  measurementId: "G-MSRQ48X3N8"
 };
 
 // Initialize Firebase
